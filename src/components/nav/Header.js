73870e2ce3feb6f6ce0e0962be2bdae220a6c42e
Hritik Chauhan <hritik@Hritiks-MacBook-Air.local>
2022-05-25 14:07:11 +0530

    navbar added

---
+++ src/components/nav/Header.js
@@ -0,0 +1,56 @@
+import { AppstoreOutlined, MailOutlined, SettingOutlined , UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import { useState } from 'react';
+import {Link} from 'react-router-dom';
+
+const items = [
+  {
+    label: 'Home',
+    key: 'home',
+    icon: <AppstoreOutlined />,
+  },
+  {
+    label: 'Username',
+    key: 'username',
+    icon: <SettingOutlined />,
+    children: [
+        {
+            label: 'Option 1',
+            key: 'setting:1',
+          },
+          {
+            label: 'Option 2',
+            key: 'setting:2',
+          },
+    ],
+
+  },
+  {
+    label: 'Register',
+    key: 'register',
+    icon: <UserAddOutlined/>,
+    style :{ marginLeft: 'auto' },
+    
+  },
+  {
+    label: 'Login',
+    key: 'login',
+    icon: <UserOutlined/>,
+    
+
+  },
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState('home');
+
+  const onClick = (e) => {
+    console.log('click ', e);
+    <Link to="/"></Link>
+    setCurrent(e.key);
+  };
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+};
+
+export default Header;
---
+++ src/pages/auth/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Register = () => {
+    return (
+        <div>
+            <p> home </p>
+        </div>
+    )
+}
+
+
+
+export default Register ; 
